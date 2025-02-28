83f87a1474c3a2ddb69bdde30150568d30a8a935
Youngrok Park <yrpark99@gmail.com>
2025-02-28 14:14:28 +0900

    Use current working directory instead of .nvim sub-directory, delete JSON file in case empty marks

--- lua/navimark/persistence.lua
+++ lua/navimark/persistence.lua
@@ -1,11 +1,16 @@
 local M = {}
 
-local local_nvim_dir = vim.fs.joinpath(vim.fn.getcwd(), ".nvim")
+local local_nvim_dir = vim.fn.getcwd()
 local persist_file = vim.fs.joinpath(local_nvim_dir, "navimark.json")
 
 M.save = function(stacks)
+  for _, stack in ipairs(stacks) do
+    if vim.tbl_isempty(stack.marks) then
+      vim.fn.delete(persist_file, "rf")
+      return
+    end
+  end
   local json = vim.fn.json_encode(stacks)
-  vim.fn.mkdir(local_nvim_dir, "p")
   vim.fn.writefile({ json }, persist_file)
 end
 
